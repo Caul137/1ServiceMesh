96f09c704a1c9e66cd22bc6627241b30109a3620
caul137 <salomao.caua@gmail.com>
2025-12-15 18:53:28 -0300

    new

--- app-ts/src/health/health.service.ts
+++ app-ts/src/health/health.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
-   console.log("Saude da aplicação ok")
+   console.log("Saude da aplicação ok -v2")
     return 'ok!';
   }
 
 
  checkReady(): string {
-    console.log("Saude da aplicação ok")
+    console.log("Saude da aplicação ok -v2")
     return 'ok!';
   }
 
